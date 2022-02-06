1a8bf3789fd1228ac9f68642475d3ed8caa54d3b
danieljaraba <daesjaga2003@gmail.com>
2022-02-05 23:29:14 -0500

    Creating user pages

---
+++ Data/RPCompetitiveProgramationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RPCompetitiveProgramation.Models;
+
+namespace RPCompetitiveProgramation.Data
+{
+    public class RPCompetitiveProgramationContext : DbContext
+    {
+        public RPCompetitiveProgramationContext (DbContextOptions<RPCompetitiveProgramationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RPCompetitiveProgramation.Models.User> User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPCompetitiveProgramation.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+    }
+}
