8474cfef2b3d08ba935d9ddc17ad61748cf05334
danieljaraba <daesjaga2003@gmail.com>
2022-02-06 00:15:16 -0500

    Adding validations

--- Models/User.cs
+++ Models/User.cs
@@ -7,12 +7,21 @@ namespace RPCompetitiveProgramation.Models
     public class User
     {
         public int ID { get; set; }
-        [Display(Name = "User Name")]
+        [Display(Name = "User name")]
+        [StringLength(60, MinimumLength = 4)]
+        [Required]
         public string UserName { get; set; }
+        [StringLength(60, MinimumLength = 8)]
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Display(Name = "First Name")]
+        [Display(Name = "First name")]
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
         public string FirstName { get; set; }
-        [Display(Name = "Last Name")]
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
         [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
