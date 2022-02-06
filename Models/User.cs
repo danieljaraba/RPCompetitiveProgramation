791542cf0efd00eefa1390a08e2a2d2b0a541d4c
danieljaraba <daesjaga2003@gmail.com>
2022-02-05 23:45:52 -0500

    Updating visuals

--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RPCompetitiveProgramation.Models
 {
     public class User
     {
         public int ID { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         public string Password { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
